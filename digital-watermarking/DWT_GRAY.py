6df88df722d13f6460ba45aba5ada840e3b3870f
31shubhmganvir <36896500+31shubhmganvir@users.noreply.github.com>
2023-10-03 20:34:09 +0530

    Update DWT_GRAY.py

--- digital-watermarking/DWT_GRAY.py
+++ digital-watermarking/DWT_GRAY.py
@@ -102,8 +102,8 @@ def read_file(path, color):  # color == GRAY or RGB
 
 # # Run dwt GRAY_LL, and dwt RGB_LL methods
 if __name__ == "__main__":
-    coverImagePath = '/content/digital-watermarking/images/mandrill_512.jpg'
-    watermarkImagePath = '/content/digital-watermarking/images/lenna_512.jpg'
+    coverImagePath = '/content/Mini_Project_MTech/digital-watermarking/images/mandrill_512.jpg'
+    watermarkImagePath = '/content/Mini_Project_MTech/digital-watermarking/images/lenna_512.jpg'
 #
 #     watermarked_img_gray = DWT_GRAY_LL_EMBED(coverImagePath, watermarkImagePath)
 #     extracted_img_gray = DWT_GRAY_LL_EXTRACT(coverImagePath, watermarkImagePath,
